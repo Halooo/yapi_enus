d3e277853413bd1094141307bcc6432e11f3533c
Hao Sun <hao.sun@futureadivsor.com>
2018-05-31 16:31:43 -0700

    translate plugin for export

--- exts/yapi-plugin-export-data/client.js
+++ exts/yapi-plugin-export-data/client.js
@@ -8,17 +8,17 @@ function exportData(exportDataModule,pid){
     exportDataModule.html = {
         name: 'html',
         route: `/api/plugin/export?type=html&pid=${pid}`,
-        desc: '导出项目接口文档为 html 文件'
+        desc: 'Export APIs as html document'
     }
     exportDataModule.markdown = {
         name: 'markdown',
         route: `/api/plugin/export?type=markdown&pid=${pid}`,
-        desc: '导出项目接口文档为 markdown 文件'
+        desc: 'Export APIs as markdown document'
     },
     exportDataModule.json = {
         name: 'json',
         route: `/api/plugin/export?type=json&pid=${pid}`,
-        desc: '导出项目接口文档为 json 文件,可使用该文件导入接口数据'
+        desc: 'Export APIs as json document, this can be imported by other users'
     }
     // exportDataModule.pdf = {
     //     name: 'pdf',
